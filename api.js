d7a7898c0043dc95874978432bafd03d3a1b8a1d
Harsh Bhatt <harshbhatt0151@gmail.com>
2022-11-09 18:08:33 +0530

    added apis

--- api.js
+++ api.js
@@ -11,8 +11,9 @@ client.connect();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get('/blocks/:blocks', (req, res)=>{
-    client.query(`Select * from blocks`, (err, result)=>{
+//get block by block number
+app.get('/blocks/:number', (req, res)=>{
+    client.query(`Select * from blocks where number = ${req.params.number}`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
@@ -29,8 +30,9 @@ app.get('/logs', (req, res)=>{
     client.end;
 })
 
-app.get('/token_transfers', (req, res)=>{
-    client.query(`Select * from token_transfers`, (err, result)=>{
+//get token transfer from particular token address
+app.get('/token_transfers/:token_address', (req, res)=>{
+    client.query(`Select * from token_transfers where token_address = '${req.params.token_address}'`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
@@ -38,21 +40,44 @@ app.get('/token_transfers', (req, res)=>{
     client.end;
 })
 
-app.get('/traces', (req, res)=>{
+
+
+//get transaction by transaction hash
+app.get('/transactions/:hash', (req, res)=>{
+    client.query(`Select * from transactions where hash = '${req.params.hash}'`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+//outgoing transactions
+app.get('/transactions/add/:from_address', (req, res)=>{
+    client.query(`Select * from transactions where from_address = '${req.params.from_address}'`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+//incoming transactions
+app.get('/transactions/add/:to_address', (req, res)=>{
+    client.query(`Select * from transactions where to_address = '${req.params.to_address}'`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+/*app.get('/traces', (req, res)=>{
     client.query(`Select * from traces`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
     });
     client.end;
-})
-
-app.get('/transactions', (req, res)=>{
-    client.query(`Select * from transactions`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-})
+})*/
 
